36402f01f0928c1c5f972e573386b0a402464cfd
umbertochimenti <60193129+umbertochimenti@users.noreply.github.com>
2020-02-13 08:13:01 +0100

    Update esp_web_sockets_format.ino

--- esp_web_sockets_base/esp_web_sockets_format.ino
+++ esp_web_sockets_base/esp_web_sockets_format.ino
@@ -12,65 +12,70 @@ const char* password = "hotspot_password";
 using namespace websockets;
 WebsocketsServer server;
 
-void wifi_connection() {
-  
+void setup() {
+  wifiInit();
+  wsServerInit();
+  carInit();
+  carStop();
+}
+
+void wifiInit() {
+  Serial.begin(9600);
   WiFi.begin(ssid, password);
-  for (int i = 0; i < 15 && WiFi.status() != WL_CONNECTED; i++) {
+  
+  for(int i = 0; i < 15 && WiFi.status() != WL_CONNECTED; i++) {
       Serial.print(".");
       delay(1000);
   }
+  
   Serial.println("");
   Serial.println("WiFi connected");
   Serial.println("IP address: ");
   Serial.println(WiFi.localIP());
+}
+
+void wsServerInit() {
   server.listen(8000);
-  Serial.print("[INFO] server available: ");
+  Serial.print("Is server live? ");
   Serial.println(server.available());
 }
 
-void init_dc_motor() {
+void carInit() {
   pinMode(D1, OUTPUT);
   pinMode(D2, OUTPUT);
   pinMode(D3, OUTPUT);
   pinMode(D4, OUTPUT);
-  stop_motor();
 }
 
-void setup() {
-  Serial.begin(9600);
-  wifi_connection();
-  init_dc_motor();
-}
-
-void come_on() {
+void carOn() {
   digitalWrite(D1, HIGH);
-  digitalWrite(D4, HIGH);
   digitalWrite(D2, LOW);
-  digitalWrite(D3, LOW);
-}
-
-void back() {
-  digitalWrite(D2, HIGH);
   digitalWrite(D3, HIGH);
-  digitalWrite(D1, LOW);
   digitalWrite(D4, LOW);
 }
 
-void left() {
-  digitalWrite(D2, HIGH);
-  digitalWrite(D4, HIGH);
+void carDown() {
   digitalWrite(D1, LOW);
+  digitalWrite(D2, HIGH);
   digitalWrite(D3, LOW);
+  digitalWrite(D4, HIGH);
 }
 
-void right() {
-  digitalWrite(D1, HIGH);
+void carRight() {
+  digitalWrite(D1, LOW);
+  digitalWrite(D2, HIGH);
   digitalWrite(D3, HIGH);
-  digitalWrite(D2, LOW);
   digitalWrite(D4, LOW);
 }
 
-void stop_motor() {
+void carLeft() {
+  digitalWrite(D1, HIGH);
+  digitalWrite(D2, LOW);
+  digitalWrite(D3, LOW);
+  digitalWrite(D4, HIGH);
+}
+
+void carStop() {
   digitalWrite(D1, LOW);
   digitalWrite(D2, LOW);
   digitalWrite(D3, LOW);
@@ -78,27 +83,28 @@ void stop_motor() {
 }
 
 void loop() {
+  
   WebsocketsClient client = server.accept();
+  
   if (client.available()) {
     
     WebsocketsMessage msg = client.readBlocking();
-    Serial.print("Got Message: ");
-    Serial.println(msg.data());
+    String robotMsg = msg.data();
+    Serial.print("[INFO] Robot-car msg: ");
+    Serial.println(robotMsg);
 
-    if (msg.data() == "command|on#") {
-      come_on();
-    } else if (msg.data() == "command|back#") {
-      back();
-    } else if (msg.data() == "command|left#") {
-      left();
-    } else if (msg.data() == "command|right#") {
-      right();
-    } else if (msg.data() == "command|stop#") {
-      stop_motor();
+    if (robotMsg == "command|on#") {
+      carOn();
+    } else if (robotMsg == "command|back#") {
+      carDown();
+    } else if (robotMsg == "command|left#") {
+      carLeft();
+    } else if (robotMsg == "command|right#") {
+      carRight();
+    } else if (robotMsg == "command|stop#") {
+      carStop();
     }
-    
-    client.send("Echo: " + msg.data());
     client.close();
   }
-  delay(100);
+  delay(50);
 }
