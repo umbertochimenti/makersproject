cc6399e2c718eac11977e56f0a0b449b4848f4da
umbertochimenti <60193129+umbertochimenti@users.noreply.github.com>
2020-04-19 16:43:56 +0200

    Add files via upload

---
+++ ctrl_led_from_wifi/server/ctrl_led_from_wifi_ws_server.js
@@ -0,0 +1,39 @@
+var WebSocketServer = require('websocket').server;
+var http = require('http');
+var server = http.createServer(function(request, response) { });
+
+server.listen(4500, function() { 
+	console.log('[INFO] server ws listen on 4500!');
+});
+
+wsServer = new WebSocketServer({
+    httpServer: server
+});
+
+
+var five = require("johnny-five"), board = new five.Board();
+board.on("ready", function() {
+
+	var led = new five.Led(13);
+	
+	wsServer.on('request', function(request) {
+		
+		var connection = request.accept(null, request.origin);
+		
+		connection.on('message', function(message) {
+			if (message.type === 'utf8') {
+				var clientMsg = message.utf8Data;
+				console.log('[INFO] client msg: ' + clientMsg);
+				if (clientMsg == "led|on#") {
+					led.on();
+				} else if (clientMsg == "led|off#") {
+					led.off();
+				}
+			}
+		});
+		
+		connection.on('close', function(connection) {
+			console.log('[INFO] client close!');
+		});
+	});
+});
