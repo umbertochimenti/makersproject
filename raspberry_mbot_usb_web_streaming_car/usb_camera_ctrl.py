3da7edfd83b74ec78cf1adaa92537cb38dada20c
umbertochimenti <60193129+umbertochimenti@users.noreply.github.com>
2020-10-21 20:46:45 +0200

    raspberry_mbot_usb_web_streaming_car
    raspberry_mbot_usb_web_streaming_car/

---
+++ raspberry_mbot_usb_web_streaming_car/usb_camera_ctrl.py
@@ -0,0 +1,14 @@
+import cv2
+
+class VideoCamera(object):
+    def __init__(self):
+        self.video = cv2.VideoCapture(-1)
+        print("[INFO] start to capture camera from OpenCV ...")
+
+    def __del__(self):
+        self.video.release()
+
+    def get_frame(self):
+        _, image = self.video.read()
+        _, jpeg = cv2.imencode('.jpg', image)
+        return jpeg.tobytes()
